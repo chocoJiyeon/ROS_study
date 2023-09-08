b22751ebc3bd93693d999b66ae5dd7ad42d54d5b
cona <chococo1121@gmail.com>
2023-09-08 12:02:12 +0900

    update

---
+++ week2_image_transport/include/week2_image_transport/image_publisher.h
@@ -0,0 +1,18 @@
+#include <ros/ros.h>
+#include <opencv2/opencv.hpp>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <std_msgs/UInt8MultiArray.h>
+#include <iostream>
+#include <vector>
+
+
+class Sender
+{
+    public:
+        Sender();
+
+    private:
+        
+};
---
+++ week2_image_transport/include/week2_image_transport/image_subscriber.h
@@ -0,0 +1,17 @@
+#include <ros/ros.h>
+#include <opencv2/opencv.hpp>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <std_msgs/UInt8MultiArray.h>
+#include <vector>
+
+
+class Receiver
+{
+    public:
+        
+
+    private:
+        ros::NodeHandle* n_;
+};
